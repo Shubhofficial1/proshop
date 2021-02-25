18c640dc8f0b20445d10ff161997fdd74bac119b
Shubhofficial1 <Shubhofficial1@gmail.com>
2021-02-25 17:51:26 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Footer from './component/Footer'
+import Header from './component/Header'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen'
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App
